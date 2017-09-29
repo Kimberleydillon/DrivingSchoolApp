fc2ac3732852d0c712a99cb2e97de60133221cec
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2017-09-29 12:25:28 -0700

    Add Ripple effect with on-click
    Doesnt work quite as well as I would like.
    Will work on it in future version.
    Logic is there for ripple on car_view.

--- app/src/main/java/com/example/topgundrivingschool/MainActivity.java
+++ app/src/main/java/com/example/topgundrivingschool/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.topgundrivingschool;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,13 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        CardView cardView=(CardView)findViewById(R.id.card_view);
-//        cardView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Log.d("","click");
-//            }
-//        });
+        CardView cardView=(CardView)findViewById(R.id.card_view);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("","click");
+            }
+        });
     }
 }
 
