97a0fa340b6d56e586739a2f60f11ba74c8c0b94
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2017-09-29 12:39:31 -0700

    Refractor
    Take out commented code.
    Remove ripple effect for on click.

--- app/src/main/java/com/example/topgundrivingschool/MainActivity.java
+++ app/src/main/java/com/example/topgundrivingschool/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.topgundrivingschool;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
-import android.util.Log;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CardView cardView=(CardView)findViewById(R.id.card_view);
-        cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("","click");
-            }
-        });
     }
 }
 
